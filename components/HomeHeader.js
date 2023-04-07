b9653a160710d3db94704d77bd600a5b55c7f0fc
Gilberto Martini <gilberto.madeira@outlook.com>
2023-04-07 11:46:07 -0300

    separa componente de header

---
+++ components/HomeHeader.js
@@ -0,0 +1,25 @@
+import { Image, Text, View } from "react-native";
+import * as Icons from "react-native-heroicons/outline";
+
+export default function HomeHeader() {
+  return (
+    <View>
+      <View className="flex-row pb-3 items-center mx-2 ">
+        <Image
+          source={{
+            uri: "https://links.papareact.com/wru",
+          }}
+          className="h-7 w-7 bg-gray-300 p-4 rounded-full"
+        />
+        <View className="flex-1">
+          <Text className="font-bold text-gray-400 text-xs">Deliver now!</Text>
+          <Text className="font-bold text-xl">
+            Current Location
+            <Icons.ChevronDownIcon size={30} color="#0CB" />
+          </Text>
+        </View>
+        <Icons.UserIcon size={35} color="#0CB" />
+      </View>
+    </View>
+  );
+}
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -2,8 +2,7 @@ import { useNavigation } from "@react-navigation/native";
 import { useLayoutEffect } from "react";
 import { Image, View } from "react-native";
 import { SafeAreaView, Text } from "react-native";
-
-import * as Icons from "react-native-heroicons/outline";
+import HomeHeader from "../components/HomeHeader";
 
 export default function HomeScreen() {
   const navigation = useNavigation();
@@ -17,26 +16,7 @@ export default function HomeScreen() {
   return (
     <SafeAreaView className="bg-white pt-10 ">
       {/* Header */}
-      <View>
-        <View className="flex-row pb-3 items-center mx-2 ">
-          <Image
-            source={{
-              uri: "https://links.papareact.com/wru",
-            }}
-            className="h-7 w-7 bg-gray-300 p-4 rounded-full"
-          />
-          <View className="flex-1">
-            <Text className="font-bold text-gray-400 text-xs">
-              Deliver now!
-            </Text>
-            <Text className="font-bold text-xl">
-              Current Location.
-              <Icons.ChevronDownIcon size={30} color="#0CB" />
-            </Text>
-          </View>
-          <Icons.UserIcon size={35} color="#0CB" />
-        </View>
-      </View>
+      <HomeHeader />
     </SafeAreaView>
   );
 }
