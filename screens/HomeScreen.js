a4b7195628cca491fb699eafea5527649f15d7aa
Gilberto Martini <gilberto.madeira@outlook.com>
2023-04-07 11:37:01 -0300

    termina o header

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -3,6 +3,8 @@ import { useLayoutEffect } from "react";
 import { Image, View } from "react-native";
 import { SafeAreaView, Text } from "react-native";
 
+import * as Icons from "react-native-heroicons/outline";
+
 export default function HomeScreen() {
   const navigation = useNavigation();
 
@@ -13,18 +15,28 @@ export default function HomeScreen() {
     });
   }, []);
   return (
-    <SafeAreaView>
-      <Text className="text-red-500">
-        {/* Header */}
-        <View>
+    <SafeAreaView className="bg-white pt-10 ">
+      {/* Header */}
+      <View>
+        <View className="flex-row pb-3 items-center mx-2 ">
           <Image
             source={{
               uri: "https://links.papareact.com/wru",
             }}
             className="h-7 w-7 bg-gray-300 p-4 rounded-full"
           />
+          <View className="flex-1">
+            <Text className="font-bold text-gray-400 text-xs">
+              Deliver now!
+            </Text>
+            <Text className="font-bold text-xl">
+              Current Location.
+              <Icons.ChevronDownIcon size={30} color="#0CB" />
+            </Text>
+          </View>
+          <Icons.UserIcon size={35} color="#0CB" />
         </View>
-      </Text>
+      </View>
     </SafeAreaView>
   );
 }
